42c9de4c35344ed3be2040ab00223bdfb778b40a
Tinashe Walter Zulu <tinzulu@gmail.com>
2017-07-10 20:55:49 +0200

    modifying the mailmodel required maessages

--- sendMail/Models/MailModel.cs
+++ sendMail/Models/MailModel.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace sendMail.Models
 {
     public class MailModel
     {
         public string From { get; set; }
+
+        [Required(ErrorMessage = "Please insert mail to address")]
         public string To { get; set; }
+        [Required(ErrorMessage = "Please insert the subject")]
         public string Subject { get; set; }
         public string Body { get; set; }
     }
